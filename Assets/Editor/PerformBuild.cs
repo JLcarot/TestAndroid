eca80da55ea3f273ca24743724072ebf490f5d4f
JohanL <129319056+JLcarot@users.noreply.github.com>
2023-06-07 12:48:27 +0200

    Create PerformBuild.cs

---
+++ Assets/Editor/PerformBuild.cs
@@ -0,0 +1,20 @@
+[UnityEditor.MenuItem("CUSTOM/Test Android Build Step")]
+static void androidBuild ()
+{
+    Debug.Log("Command line build android version\n------------------\n------------------");
+
+    string[] scenes = GetBuildScenes();
+    string path = GetBuildPathAndroid();
+    if(scenes == null || scenes.Length==0 || path == null)
+        return;
+
+    Debug.Log(string.Format("Path: \"{0}\"", path));
+    for(int i=0; i<scenes.Length; ++i)
+    {
+        Debug.Log(string.Format("Scene[{0}]: \"{1}\"", i, scenes[i]));
+    }
+
+    Debug.Log("Starting Android Build!");
+    BuildPipeline.BuildPlayer(scenes, path, BuildTarget.Android, BuildOptions.None);
+    BuildPipeline.buil
+}
