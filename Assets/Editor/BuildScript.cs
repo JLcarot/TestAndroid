bb7d77475206597c1c414d4c4bf6b652f04cee13
JohanL <129319056+JLcarot@users.noreply.github.com>
2023-06-15 18:02:52 +0200

    Update BuildScript.cs

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -5,53 +5,20 @@ using System.Collections.Generic;
 class BuildScript {
 	static string[] SCENES = FindEnabledEditorScenes();
 
-	static string APP_NAME = "AngryBots";
-	static string TARGET_DIR = "target";
+	static string APP_NAME = "Test_Android";
+	static string TARGET_DIR = "C:/";
 
 	static void PerformAllBuilds ()
 	{
-		PerformMacOSXBuild ();
 		PerformWindowsBuild ();
-		PerformWebStreamingBuild ();
-		PerformWebBuild ();
 		PerformAndroidBuild();
 		PerformiOSBuild();
-	}
-
-	static void PerformMacOSXBuild ()
-	{
-		string target_dir = APP_NAME;
-		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneOSXUniversal,BuildOptions.None);
 	}
 
 	static void PerformWindowsBuild ()
 	{
 		string target_dir = APP_NAME + ".exe";
-		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows,BuildOptions.None);
-	}
-
-	static void PerformLinuxBuild ()
-	{
-		string target_dir = "linux";
-		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneLinux,BuildOptions.None);
-	}
-
-	static void PerformWebStreamingBuild ()
-	{
-		string target_dir = "webstreaming";
-		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.WebPlayerStreamed,BuildOptions.None);
-	}
-
-	static void PerformWebBuild ()
-	{
-		string target_dir = "web";
-		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.WebPlayer,BuildOptions.None);
-	}
-
-	static void PerformWebGLBuild ()
-	{
-		string target_dir = "webgl";
-		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.WebGL,BuildOptions.None);
+		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows,BuildOptions.None);
 	}
 
 	static void PerformAndroidBuild ()
@@ -59,7 +26,7 @@ class BuildScript {
 		//Set the path to the Android SDK on the machine, since Unity cannot retain the state properly
 		AndroidSDKFolder.Path = "${ANDROID_HOME}";
 		string target_dir = APP_NAME + ".apk";
-		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android,BuildOptions.None);
+		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTargetGroup.Android, BuildTarget.Android, BuildOptions.None);
 	}
 
 	static void PerformiOSBuild ()
@@ -67,7 +34,7 @@ class BuildScript {
 		string target_dir = "iOS";
 		//We do not build the xcodeproject in the target directory, since we do not want to archive the
 		//entire xcode project, but instead build with XCode, then output the .ipa through Jenkins
-		GenericBuild(SCENES, target_dir, BuildTarget.iOS,BuildOptions.None);
+		GenericBuild(SCENES, target_dir, BuildTargetGroup.iOS, BuildTarget.iOS, BuildOptions.None);
 	}
 
 	private static string[] FindEnabledEditorScenes() {
@@ -79,9 +46,9 @@ class BuildScript {
 		return EditorScenes.ToArray();
 	}
 
-	static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+	static void GenericBuild(string[] scenes, string target_dir, BuildTargetGroup build_target_group, BuildTarget build_target, BuildOptions build_options)
 	{
-		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target_group, build_target);
 		string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
 		if (res.Length > 0) {
 			throw new Exception("BuildPlayer failure: " + res);
