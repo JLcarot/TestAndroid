ce5dbb2f46a8e11b385203eb7f06d5763c88541a
JohanL <129319056+JLcarot@users.noreply.github.com>
2023-06-15 18:19:03 +0200

    Update BuildScript.cs

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -23,8 +23,6 @@ class BuildScript {
 
 	static void PerformAndroidBuild ()
 	{
-		//Set the path to the Android SDK on the machine, since Unity cannot retain the state properly
-		AndroidSDKFolder.Path = "${ANDROID_HOME}";
 		string target_dir = APP_NAME + ".apk";
 		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTargetGroup.Android, BuildTarget.Android, BuildOptions.None);
 	}
